85ef613c486ac2519d3418bd40f7175f0e054eef
timri86 <richard.ngu.tic@gmail.com>
2021-01-14 18:13:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', evt=>{
 self.addEventListener('activate', evt => {
     console.log(evt);
 });
-self.addEventListener('fetch', evt => {
-    if(!navigator.onLine){
-        evt.respondWith(New Response('Pas de connexion pour ce site'));
+self.addEventListener('fetch', evt=>{
+    if (!navigator.onLine){
+        evt.respondWith( new Response('pas de connexion internet'))
     }
     console.log(evt.request.url);
-});
+})
