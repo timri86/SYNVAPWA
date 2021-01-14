773b908ffeb243b1ff61f96bed1df50006f9e827
timri86 <richard.ngu.tic@gmail.com>
2021-01-14 17:10:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,7 @@
 self.addEventListener('install', evt=>{
-    caches.open('lpdwca-PWA').then(
-                cache=>{
-                    cache.addAll([
-                        'index.html',
-                        'sw.js',
-                       
-                        ]);
-                })
-
+    console.log(evt);
 });
 
 self.addEventListener('activate', evt => {
     console.log(evt);
-});
-self.addEventListener('fetch', evt=>{
-    if (!(evt.request.url.indexOf('http') === 0)) return;
-    evt.respondWith(
-        caches.match(evt.request).then(rep=>{
-            if(rep){
-                //si la page existe on la retourne
-                return rep;
-            }
-            /*si la page n'existe pas, on utilise la méthode network    fallback pour ouvrir l'instance de cache et enregistrer la page dans le cache pour les futurs requêtes
-            */
-            return fetch(evt.request).then(
-                newResponse=>{
-                    caches.open('lpdwca-PWA').then(
-                        cache=>cache.put(evt.request, newResponse
-                        ));
-                        /*puisque une réponse ne peut être utilisé 2 fois, si on a besoin de l'utiliser une seconde fois, on doit le cloner
-                        */
-                        return newResponse.clone();
-                })
-        })
-    )
-
 });
